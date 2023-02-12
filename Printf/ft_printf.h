d95a5552f51a0196b75b1aa712dc6a341923622d
Yakup Hamza Şensoy <89227314+Yakuphsensoy@users.noreply.github.com>
2023-02-12 15:35:13 +0300

    Add files via upload

---
+++ Printf/ft_printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yasensoy < yasensoy@student.42kocaeli.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/10 22:54:11 by yasensoy          #+#    #+#             */
+/*   Updated: 2023/02/10 22:54:13 by yasensoy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_printf(const char *str, ...);
+
+#endif
